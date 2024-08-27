b25c444fb64d8e0cb58db8202c4557f4532ee407
kamilgolawski <kamilgolawski3@icloud.com>
2024-08-27 22:13:04 +0200

    Initial commit

---
+++ src/main/java/skni/kamilG/skin_sensors_api/SkinSensorsApiApplication.java
@@ -0,0 +1,13 @@
+package skni.kamilG.skin_sensors_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinSensorsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkinSensorsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skni/kamilG/skin_sensors_api/SkinSensorsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package skni.kamilG.skin_sensors_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinSensorsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
