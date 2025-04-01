77d5a98b8c6bb2b025ec25248b93424c498ea5ab
kamilgolawski <kamilgolawski3@icloud.com>
2025-04-01 22:52:41 +0200

    SSE wrong path request handler

--- src/main/java/skni/kamilG/skin_sensors_api/LiveData/Controller/LiveController.java
+++ src/main/java/skni/kamilG/skin_sensors_api/LiveData/Controller/LiveController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import skni.kamilG.skin_sensors_api.LiveData.Service.ISseEmitterService;
@@ -18,7 +19,7 @@ public class LiveController {
   private final ISseEmitterService sseEmitterService;
 
   @GetMapping(value = "/{sensorId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  public SseEmitter streamSensor(
+  public ResponseEntity<SseEmitter> streamSensor(
       @PathVariable Short sensorId,
       @SuppressWarnings("unused") @RequestHeader(value = "Last-Event-ID", required = false)
           String lastEventId,
@@ -31,11 +32,15 @@ public class LiveController {
 
     String clientId = UUID.randomUUID().toString();
     log.info("New SSE connection request for client: {}, sensor: {}", clientId, sensorId);
-    return sseEmitterService.createSseEmitter(clientId, sensorId);
+    SseEmitter emitter = sseEmitterService.createSseEmitter(clientId, sensorId);
+
+    return ResponseEntity.ok()
+            .contentType(MediaType.TEXT_EVENT_STREAM)
+            .body(emitter);
   }
 
   @GetMapping(value = "/all", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  public SseEmitter streamAllSensorsUpdates(
+  public ResponseEntity<SseEmitter> streamAllSensorsUpdates(
       @SuppressWarnings("unused") @RequestHeader(value = "Last-Event-ID", required = false)
           String lastEventId,
       HttpServletResponse response) {
@@ -47,6 +52,9 @@ public class LiveController {
 
     String clientId = UUID.randomUUID().toString();
     log.info("New SSE connection request for client: {}, all sensors", clientId);
-    return sseEmitterService.createSseEmitter(clientId, null);
+    SseEmitter emitter =  sseEmitterService.createSseEmitter(clientId, null);
+    return ResponseEntity.ok()
+            .contentType(MediaType.TEXT_EVENT_STREAM)
+            .body(emitter);
   }
 }
