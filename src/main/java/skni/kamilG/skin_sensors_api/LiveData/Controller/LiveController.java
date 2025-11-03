5ff3ef0f068e44b47a6ff3973e564b64494d9eb2
kamilgolawski <kamilgolawski3@icloud.com>
2025-11-03 23:39:51 +0100

    LiveController threads management changed

--- src/main/java/skni/kamilG/skin_sensors_api/LiveData/Controller/LiveController.java
+++ src/main/java/skni/kamilG/skin_sensors_api/LiveData/Controller/LiveController.java
@@ -1,14 +1,18 @@
 package skni.kamilG.skin_sensors_api.LiveData.Controller;
 
-import jakarta.servlet.http.HttpServletResponse;
-import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import skni.kamilG.skin_sensors_api.LiveData.Service.ISseEmitterService;
+
+import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -16,41 +20,38 @@ import skni.kamilG.skin_sensors_api.LiveData.Service.ISseEmitterService;
 @RequestMapping("/live-api/sensors")
 public class LiveController {
 
-  private final ISseEmitterService sseEmitterService;
+    private final ISseEmitterService sseEmitterService;
 
-  @GetMapping(value = "/{sensorId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  public ResponseEntity<SseEmitter> streamSensor(
-      @PathVariable Short sensorId,
-      @SuppressWarnings("unused") @RequestHeader(value = "Last-Event-ID", required = false)
-          String lastEventId,
-      HttpServletResponse response) {
-    response.setHeader("Cache-Control", "no-store");
-    response.setHeader("X-Accel-Buffering", "no");
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Connection", "keep-alive");
-    response.setHeader("Content-Type", "text/event-stream;charset=UTF-8");
+    @GetMapping(value = "/{sensorId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> streamSensor(
+            @PathVariable Short sensorId,
+            @SuppressWarnings("unused") @RequestHeader(value = "Last-Event-ID", required = false)
+            String lastEventId) {
 
-    String clientId = UUID.randomUUID().toString();
-    log.debug("New SSE connection request for client: {}, sensor: {}", clientId, sensorId);
-    SseEmitter emitter = sseEmitterService.createSseEmitter(clientId, sensorId);
+        String clientId = UUID.randomUUID().toString();
+        log.debug("New SSE connection request for client: {}, sensor: {}", clientId, sensorId);
+        SseEmitter emitter = sseEmitterService.createSseEmitter(clientId, sensorId);
 
-    return ResponseEntity.ok().contentType(MediaType.TEXT_EVENT_STREAM).body(emitter);
-  }
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_EVENT_STREAM)
+                .header("Cache-Control", "no-store")
+                .header("X-Accel-Buffering", "no")
+                .header("Connection", "keep-alive")
+                .body(emitter);
+    }
 
-  @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  public ResponseEntity<SseEmitter> streamAllSensorsUpdates(
-      @SuppressWarnings("unused") @RequestHeader(value = "Last-Event-ID", required = false)
-          String lastEventId,
-      HttpServletResponse response) {
-    response.setHeader("Cache-Control", "no-store");
-    response.setHeader("X-Accel-Buffering", "no");
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Connection", "keep-alive");
-    response.setHeader("Content-Type", "text/event-stream;charset=UTF-8");
-
-    String clientId = UUID.randomUUID().toString();
-    log.info("New SSE connection request for client: {}, all sensors", clientId);
-    SseEmitter emitter = sseEmitterService.createSseEmitter(clientId, null);
-    return ResponseEntity.ok().contentType(MediaType.TEXT_EVENT_STREAM).body(emitter);
-  }
+    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> streamAllSensorsUpdates(
+            @SuppressWarnings("unused") @RequestHeader(value = "Last-Event-ID", required = false)
+            String lastEventId) {
+        String clientId = UUID.randomUUID().toString();
+        log.info("New SSE connection request for client: {}, all sensors", clientId);
+        SseEmitter emitter = sseEmitterService.createSseEmitter(clientId, null);
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_EVENT_STREAM)
+                .header("Cache-Control", "no-store")
+                .header("X-Accel-Buffering", "no")
+                .header("Connection", "keep-alive")
+                .body(emitter);
+    }
 }
